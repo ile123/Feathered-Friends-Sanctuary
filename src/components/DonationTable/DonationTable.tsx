27bc7fa69d0068e24580c239a9aff7d95ceda820
Ilario Batistic <ilario.batistic@gmail.com>
2023-05-05 20:02:12 +0200

    halfway done with donations

--- src/components/DonationTable/DonationTable.tsx
+++ src/components/DonationTable/DonationTable.tsx
@@ -8,16 +8,23 @@ export default function DonationTable(props: any) {
     return(
         <>
             <table id={styles.table}>
-                <thead>
+                <thead id={styles.head}>
                     <tr>
-                        <th>Type</th>
-                        <th>Value</th>
-                        <th>Description</th>
+                        <th className={styles.headItem}>#</th>
+                        <th className={styles.headItem}>Type</th>
+                        <th className={styles.headItem}>Value</th>
+                        <th className={styles.headItem}>Description</th>
+                        <th className={styles.headItem}>Options</th>
                     </tr>
                 </thead>
                 <tbody>
                     {donations.map((donation: any, index: number) => {
-                        <DonationTableRow key={index}  />
+                        <DonationTableRow 
+                            key={index} 
+                            id={donation.id} 
+                            type={donation.type} 
+                            value={donation.value} 
+                            description={donation.description} />
                     })}
                 </tbody>
             </table>
